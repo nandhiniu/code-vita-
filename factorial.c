a43bb55662704de0e2e70377e9368555a5464b31
nandhiniu <30337570+nandhiniu@users.noreply.github.com>
2017-07-24 14:47:54 +0530

    factorial.c

---
+++ factorial.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i,temp=1,n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        temp=temp*i;
+        
+    }
+    printf("%d",temp);
+        
+    return 0;
+}
